934147dcc28574cc6a65038572b50ddf0d24f8a2
Matthew <Matt2.Steele@live.uwe.ac.uk>
2025-12-16 16:29:56 +0000

    Offset back to working!
    Had issues with offsets not working again, simplified code drastically and put it in a lateupdate function as the offset was technically working but it was being set before it was spawned and ended up back in the original position which is half way in the plane

--- Matt AT AR Final/Assets/MyAdditions/Scripts/Offsets.cs
+++ Matt AT AR Final/Assets/MyAdditions/Scripts/Offsets.cs
@@ -2,8 +2,29 @@ using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Samples.ARStarterAssets;
 
 public class Offsets : MonoBehaviour
 {
-    public Vector3 offset = new Vector3(0, 0.2f, 0);
+    public Vector3 offset = new Vector3();
+    private bool applied;
+
+    
+
+    private void LateUpdate()
+    {
+        if (applied)
+        {
+            Debug.Log("Somethings not right here");
+            return;
+        }
+        applied = true;
+        transform.position += transform.rotation * offset;
+        Debug.Log("Should have offset!");
+
+    }
+
+    
+
+ 
 }
