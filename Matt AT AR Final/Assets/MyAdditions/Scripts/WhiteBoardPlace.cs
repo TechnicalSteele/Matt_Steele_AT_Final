453824b0bd2c28f7f5e610800fd141c34b7cbad0
Matthew <Matt2.Steele@live.uwe.ac.uk>
2025-12-16 01:49:27 +0000

    adding spawn previews

--- Matt AT AR Final/Assets/MyAdditions/Scripts/WhiteBoardPlace.cs
+++ Matt AT AR Final/Assets/MyAdditions/Scripts/WhiteBoardPlace.cs
@@ -1,9 +1,18 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class WhiteBoardPlace : MonoBehaviour
 {
+
+    [SerializeField] private InputActionReference confirmPress;
+    [SerializeField] private GameObject WhiteboardSpawn;
+    [SerializeField] private GameObject Preview;
+
+    private bool confirmed;
+
     XRGrabInteractable interactable;
 
     private void Awake()
@@ -14,21 +23,36 @@ public class WhiteBoardPlace : MonoBehaviour
     public  void OnEnable()
     {
         Debug.Log("Please?");
-        interactable.selectEntered.AddListener(confirmWhiteBoard);
+        //HoldInteraction. -= confirmWhiteBoard;
+        confirmPress.action.Enable();
+
     }
     private void OnDisable()
     {
-        interactable.selectEntered.RemoveListener(confirmWhiteBoard);
+        confirmPress.action.performed -= confirmWhiteBoard;
     }
-    private void OnDestroy()
+    
+    
+    /*private void OnDestroy()
     {
         Debug.Log("Destroyed!!");
     }
+    purely for testing to see if script was implementing correctly
+   */ 
 
 
-    void confirmWhiteBoard(SelectEnterEventArgs args)
+
+    void confirmWhiteBoard(InputAction.CallbackContext callback)
     {
+        if(confirmed)
+        {
+            return;
+        }
+        confirmed = true;
         Debug.Log("It Works!");
+
+        Instantiate(WhiteboardSpawn, transform.position,transform.rotation);
+        Destroy(Preview);
     }
 
 }
